4c8fd5377c8dc6abe4569c65d4d765d426796e76
Daniel <kopylenkod@gmail.com>
2024-07-23 22:32:09 -0400

    Added new member and shadow under navbar

--- app/members/members.js
+++ app/members/members.js
@@ -1,44 +1,51 @@
 export const members = [
   {
-    name: "Elaine & Bob Gleason",
-    cattery: "Hullabaloo",
-    breed: "Burmese",
-    email: "robertgleason@rogers.com",
+    name: 'Elaine & Bob Gleason',
+    cattery: 'Hullabaloo',
+    breed: 'Burmese',
+    email: 'robertgleason@rogers.com',
   },
 
   {
-    name: "Andrey Kopylenko",
-    cattery: "Aurore de la Vie",
-    breed: "British Shorthair",
-    email: "kopylenkoa@gmail.com",
-    link: "https://www.auroredelavie.com",
+    name: 'Andrey Kopylenko',
+    cattery: 'Aurore de la Vie',
+    breed: 'British Shorthair',
+    email: 'kopylenkoa@gmail.com',
+    link: 'https://www.auroredelavie.com',
   },
 
   {
-    name: "Karen Miksza",
-    cattery: "Mikkara",
-    breed: "Japanese Bobtails, Cornish Rex",
-    email: "mikkarakats@gmail.com",
+    name: 'Karen Miksza',
+    cattery: 'Mikkara',
+    breed: 'Japanese Bobtails, Cornish Rex',
+    email: 'mikkarakats@gmail.com',
   },
 
   {
-    name: "Barb & David Sanderson",
-    cattery: "",
-    breed: "Maine Coon Alters",
-    email: "dbsand@rogers.com",
+    name: 'Barb & David Sanderson',
+    cattery: '',
+    breed: 'Maine Coon Alters',
+    email: 'dbsand@rogers.com',
   },
 
   {
-    name: "Aleida Sanderson-Bagchus",
-    cattery: "Petitspaws",
-    breed: "Balinese",
-    email: "petitspawscats@gmail.com",
+    name: 'Aleida Sanderson-Bagchus',
+    cattery: 'Petitspaws',
+    breed: 'Balinese',
+    email: 'petitspawscats@gmail.com',
   },
 
   {
-    name: "Kathy Early & Derrick Arndt",
-    cattery: "",
-    breed: "Household Pets",
-    email: "gogokat@live.ca",
+    name: 'Kathy Early & Derrick Arndt',
+    cattery: '',
+    breed: 'Household Pets',
+    email: 'gogokat@live.ca',
+  },
+
+  {
+    name: 'Allison Meiwald',
+    cattery: '',
+    breed: 'Russian Blues',
+    email: 'ameiwald@gmail.com',
   },
 ];
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
 
-import logo from "@/public/assets/HCFLogo.webp";
-import close from "@/public/assets/close.svg";
-import menu from "@/public/assets/menu.svg";
+import logo from '@/public/assets/HCFLogo.webp';
+import close from '@/public/assets/close.svg';
+import menu from '@/public/assets/menu.svg';
 
 import {
   NavigationMenu,
@@ -17,54 +17,56 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
-import { Button } from "@/components/ui/button";
-import { Cat } from "lucide-react";
+} from '@/components/ui/navigation-menu';
+import { Button } from '@/components/ui/button';
+import { Cat } from 'lucide-react';
 
 const Navbar = () => {
   const [isMounted, setIsMounted] = useState(false);
   const [toggle, setToggle] = useState(false);
 
+  const router = useRouter();
+
   const menuStyle =
-    "rounded-lg text-3xl font-medium font-lora ring-offset-background w-full p-2 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primaryBg text-secondaryBg hover:bg-[#D2D2D2]/70";
+    'rounded-lg text-3xl font-medium font-lora ring-offset-background w-full p-2 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primaryBg text-secondaryBg hover:bg-[#D2D2D2]/70';
 
   const pathname = usePathname();
 
   const navInfo: { id: string; title: string }[] = [
     {
-      id: "next-show",
-      title: "Next Show",
+      id: 'next-show',
+      title: 'Next Show',
     },
     {
-      id: "frequently-asked-questions",
-      title: "Frequently Asked Questions",
+      id: 'frequently-asked-questions',
+      title: 'Frequently Asked Questions',
     },
     {
-      id: "spectator-information",
-      title: "Spectator Information",
+      id: 'spectator-information',
+      title: 'Spectator Information',
     },
     {
-      id: "map-and-directions",
-      title: "Maps & Directions",
+      id: 'map-and-directions',
+      title: 'Maps & Directions',
     },
     {
-      id: "new-vendor",
-      title: "Interested in Becoming a Vendor?",
+      id: 'new-vendor',
+      title: 'Interested in Becoming a Vendor?',
     },
     {
-      id: "vendors",
-      title: "Vendors",
+      id: 'vendors',
+      title: 'Vendors',
     },
   ];
 
   const navAbout: { id: string; title: string }[] = [
     {
-      id: "aims-and-objectives",
-      title: "Aims & Objectives",
+      id: 'aims-and-objectives',
+      title: 'Aims & Objectives',
     },
     {
-      id: "members",
-      title: "Members",
+      id: 'members',
+      title: 'Members',
     },
   ];
 
@@ -77,34 +79,34 @@ const Navbar = () => {
   }
 
   if (toggle) {
-    document.body.style.overflow = "hidden";
+    document.body.style.overflow = 'hidden';
   } else {
-    document.body.style.overflow = "auto";
+    document.body.style.overflow = 'auto';
   }
 
   return (
     <nav
-      className={`fixed top-0 bg-primaryBg w-full flex sm:flex-row flex-col items-center justify-center h-20 md:px-16 px-6 z-10`}
+      className={`fixed top-0 bg-primaryBg w-full flex sm:flex-row flex-col items-center justify-center h-20 md:px-16 px-6 z-10 shadow-lg`}
     >
-      <div className="flex justify-between items-center w-full z-20">
-        <Link href="/" className="flex flex-row items-center text-secondaryBg">
+      <div className='flex justify-between items-center w-full z-20'>
+        <Link href='/' className='flex flex-row items-center text-secondaryBg'>
           {/* <Cat size={32} className="mr-2 navHidden" /> */}
           <Image
             src={logo}
-            alt="HCF Logo"
-            className="w-[70px] mr-2"
+            alt='HCF Logo'
+            className='w-[70px] mr-2'
             onClick={() => setToggle(false)}
           />
-          <h2 className="font-lora font-semibold md:text-3xl text-2xl navHidden">
+          <h2 className='font-lora font-semibold md:text-3xl text-2xl navHidden'>
             Hamilton Cat Fanciers
           </h2>
         </Link>
 
-        <div className="flex flex-row navHidden text-secondaryBg">
-          <NavigationMenu className="md:mr-6 mr-2">
+        <div className='flex flex-row navHidden text-secondaryBg'>
+          <NavigationMenu className='md:mr-6 mr-2'>
             <NavigationMenuList>
               <NavigationMenuItem>
-                <Link href="/">
+                <Link href='/'>
                   <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                     Home
                   </NavigationMenuLink>
@@ -113,7 +115,7 @@ const Navbar = () => {
             </NavigationMenuList>
           </NavigationMenu>
 
-          <NavigationMenu className="md:mr-6 mr-2">
+          <NavigationMenu className='md:mr-6 mr-2'>
             <NavigationMenuList>
               <NavigationMenuItem>
                 <NavigationMenuTrigger>Information</NavigationMenuTrigger>
@@ -132,7 +134,7 @@ const Navbar = () => {
             </NavigationMenuList>
           </NavigationMenu>
 
-          <NavigationMenu className="md:mr-6 mr-2">
+          <NavigationMenu className='md:mr-6 mr-2'>
             <NavigationMenuList>
               <NavigationMenuItem>
                 <NavigationMenuTrigger>About Us</NavigationMenuTrigger>
@@ -151,26 +153,26 @@ const Navbar = () => {
             </NavigationMenuList>
           </NavigationMenu>
 
-          <Button>
-            <Link href="/enter-show">Enter Show</Link>
+          <Button asChild>
+            <Link href='/enter-show'>Enter Show</Link>
           </Button>
         </div>
 
         <Image
           src={toggle ? close : menu}
-          alt="menu"
-          className="w-[28px] h-[28px] object-contain cursor-pointer sm:hidden"
+          alt='menu'
+          className='w-[28px] h-[28px] object-contain cursor-pointer sm:hidden'
           onClick={() => setToggle(!toggle)}
         />
       </div>
       <div
         className={`fixed sm:hidden top-[50%] ${
-          toggle ? "translate-x-[0%]" : "translate-x-[100%]"
+          toggle ? 'translate-x-[0%]' : 'translate-x-[100%]'
         } translate-y-[-50%] flex justify-center items-center flex-col bg-primaryBg w-full h-full transition-all ease-in-out duration-300 z-10}`}
       >
         <ul>
           <li className={menuStyle} onClick={() => setToggle(false)}>
-            <Link href="/">Home</Link>
+            <Link href='/'>Home</Link>
           </li>
 
           {navInfo.map((option) => (
@@ -197,7 +199,7 @@ const Navbar = () => {
             className={`mt-4 hover: ${menuStyle}`}
             onClick={() => setToggle(false)}
           >
-            <Link href="/enter-show">Enter Show</Link>
+            <Link href='/enter-show'>Enter Show</Link>
           </li>
         </ul>
       </div>
